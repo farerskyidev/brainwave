5efbec5e0410fbee921df32371482bd8af1f53df
farerskyidev <118003342+farerskyidev@users.noreply.github.com>
2024-06-18 19:26:30 +0300

    HEADLESS WP

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import axios from 'axios';
 import Header from './pages/Header/Header';
 import Footer from './pages/Footer/Footer';
 import Features from './pages/Features/Features';
@@ -7,29 +8,43 @@ import Pricing from './pages/Pricing/Pricing';
 import HowToUse from './pages/HowToUse/HowToUse';
 import Roadmap from './pages/Roadmap/Roadmap';
 import Home from './pages/Home/Home';
-import NewAccount from './pages/NewAccount/NewAccount'; 
+import NewAccount from './pages/NewAccount/NewAccount';
+import Testimonials from './sections/Testimonials';
 
-export class App extends React.Component {
-  render() { 
-    return (
-      <div className='app-wrapper'>       
-        <BrowserRouter>
-          <div className="App"> 
-            <Header /> 
-            <Routes>
-              <Route exact path='/' element={<Home />} />
-              <Route exact path='/features' element={<Features/>}/>
-              <Route exact path='/pricing' element={<Pricing/>}/>
-              <Route exact path='/how-to-use' element={<HowToUse/>}/>  
-              <Route exact path='/roadmap' element={<Roadmap/>}/>
-              <Route exact path='/new-account' element={<NewAccount/>}/>
-            </Routes>
-            <Footer />
-          </div>
-        </BrowserRouter>
-      </div>
-    );
-  }
+function App() {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      try {
+        const response = await axios.get('http://brainwave.local/wp-json/wp/v2/posts');
+        setPosts(response.data);
+      } catch (error) {
+        console.error('Error fetching posts:', error);
+      }
+    };
+
+    fetchPosts();
+  }, []);
+
+  return (
+    <div className='app-wrapper'>
+      <BrowserRouter>
+        <div className="App">
+          <Header />
+          <Routes>
+            <Route exact path='/' element={<Home />} />
+            <Route exact path='/features' element={<Features />} />
+            <Route exact path='/pricing' element={<Pricing />} />
+            <Route exact path='/how-to-use' element={<HowToUse />} />
+            <Route exact path='/roadmap' element={<Roadmap />} />
+            <Route exact path='/new-account' element={<NewAccount />} />
+          </Routes>
+          <Footer />
+        </div>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
--- src/sections/Testimonials.js
+++ src/sections/Testimonials.js
@@ -1,120 +1,98 @@
-import React from 'react';
-import ava_1 from "../images/ava_1.png";
-import ava_2 from "../images/ava_2.png";
-import ava_3 from "../images/ava_3.png";
-import ava_4 from "../images/ava_4.png";
-import ava_5 from "../images/ava_5.png";
-import ava_6 from "../images/ava_6.png";
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
 const Testimonials = () => {
+  const [posts, setPosts] = useState([]);
+  const [categories, setCategories] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [featureImages, setFeatureImages] = useState({});
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      try {
+        const response = await axios.get('http://brainwave.local/wp-json/wp/v2/posts');
+        setPosts(response.data);
+        setLoading(false);
+        const fetchedPosts = response.data.map(post => ({
+          // id: post.id,
+          // title: post.title.rendered,
+          // content: post.content.rendered,
+          // categories: post.categories,
+          featuredImage: post._embedded && post._embedded['wp:featuredmedia'] ? post._embedded['wp:featuredmedia'][0].source_url : null
+        })); // Set loading to false once data is fetched
+      } catch (error) {
+        console.error('Error fetching posts:', error);
+        setLoading(false); // Ensure loading is set to false in case of error too
+      }
+    };
+
+    const fetchCategories = async () => {
+      try {
+        const response = await axios.get('http://brainwave.local/wp-json/wp/v2/categories');
+        setCategories(response.data);
+      } catch (error) {
+        console.error('Error fetching categories:', error);
+      }
+    };
+
+    fetchPosts();
+    fetchCategories();
+  }, []);
+
+  const getCategoryNames = (categoryIds) => {
+    const names = [];
+    categoryIds.forEach(categoryId => {
+      const category = categories.find(cat => cat.id === categoryId);
+      if (category) {
+        names.push(category.name);
+      }
+    });
+    return names.join(', ');
+  };
+
+  
+
+
   return (
     <section className='bg-color pt-28 relative overflow-hidden pb-40'>
-        <div className="mx-auto mb-16 text-center">
-          <p className='text-x text-purpleColor mb-4'>ready to get started</p>
-          <h2 className='text-5xl leading-[3.75rem] mb-6'>What the community is saying</h2>
-        </div> 
+      <div className="mx-auto mb-16 text-center">
+        <p className='text-x text-purpleColor mb-4'>Готові почати</p>
+        <h2 className='text-5xl leading-[3.75rem] mb-6'>Що говорить спільнота</h2>
+      </div> 
+      
+      {loading ? (
+        <p>Завантаження...</p>
+      ) : (
         <div className="grid-x grid-padding-x items-center py-5 relative w-1440 gap-y-8">
-          <div className="cell large-4 medium-6 small-12">
-            <article className="testimonials rounded-2xl">
-              <div className="p-10">
-                <p className='text-lg codepro-reg text-purpleColor2'>"As an engineer, I need to stay up-to-date with the latest technologies and trends. Brainwave's AI chatbot is a valuable resource for me, providing me with customized recommendations and insights that help me stay ahead of the curve. I would recommend this app to anyone looking to improve their knowledge and skills in their field."</p>
-              </div>
-              <div className="flex justify-between px-10 py-6 testimonials_bottom">
-                <div>
-                  <p className='text-lg'>David Chen</p>
-                  <p className='text-sm text-purpleColor'>Engineer</p>
+          {posts.map(post => (
+            <div key={post.id} className="cell large-4 medium-6 small-12">
+              <article className="testimonials rounded-2xl">
+                <div className="p-10">
+                  <p className='text-lg codepro-reg text-purpleColor2' dangerouslySetInnerHTML={{ __html: post.content.rendered }}></p>
                 </div>
-                <div>
-                  <img src={ava_1} alt="img" /> 
+                <div className="flex justify-between px-10 py-6 testimonials_bottom">
+                  <div>
+                    <p className='text-lg'>{post.title.rendered}</p>
+                    {post.categories.length > 0 ? (
+                      post.categories.map(categoryId => (
+                        <p key={categoryId} className='text-sm text-purpleColor'>{getCategoryNames([categoryId])}</p>
+                      ))
+                    ) : (
+                      <span className='text-sm text-purpleColor'>Без категорій</span>
+                    )} 
+                  </div>
+                  <div>
+ {post.featuredImage && (
+                  <img src={post.featuredImage} alt={post.title.rendered} className="w-full h-auto rounded-t-2xl" />
+                )}
+                  </div>
                 </div>
-              </div>
-            </article>
-          </div>
-          <div className="cell large-4 medium-6 small-12">
-            <article className="testimonials rounded-2xl">
-              <div className="p-10">
-                <p className='text-lg codepro-reg text-purpleColor2'>"I was blown away by the accuracy and speed of the AI chatbot on Brainwave. It was able to understand my queries and provide relevant recommendations in seconds."</p>
-              </div>
-              <div className="flex justify-between px-10 py-6 testimonials_bottom">
-                <div>
-                  <p className='text-lg'>David Chen</p>
-                  <p className='text-sm text-purpleColor'>Engineer</p>
-                </div>
-                <div>
-                  <img src={ava_2} alt="img" /> 
-                </div>
-              </div>
-            </article>
-          </div>
-          <div className="cell large-4 medium-6 small-12">
-            <article className="testimonials rounded-2xl">
-              <div className="p-10">
-                <p className='text-lg codepro-reg text-purpleColor2'>"I have been using Brainwave for several months and have been impressed with its capabilities. The AI chatbot is able to understand complex queries and provide accurate recommendations, making my life easier and more productive. I would recommend this app to anyone looking for a reliable and efficient AI assistant."</p>
-              </div>
-              <div className="flex justify-between px-10 py-6 testimonials_bottom">
-                <div>
-                  <p className='text-lg'>David Chen</p>
-                  <p className='text-sm text-purpleColor'>Engineer</p>
-                </div>
-                <div>
-                  <img src={ava_3} alt="img" /> 
-                </div>
-              </div>
-            </article>
-          </div>
-          <div className="cell large-4 medium-6 small-12">
-            <article className="testimonials rounded-2xl">
-              <div className="p-10">
-                <p className='text-lg codepro-reg text-purpleColor2'>"Brainwave has been a game-changer for my business. The AI chatbot helps me stay on top of industry trends and provides me with customized recommendations that help me make informed decisions. It's like having a personal assistant at my fingertips. I would highly recommend this app to any entrepreneur looking to improve their productivity and efficiency."</p>
-              </div>
-              <div className="flex justify-between px-10 py-6 testimonials_bottom">
-                <div>
-                  <p className='text-lg'>David Chen</p>
-                  <p className='text-sm text-purpleColor'>Engineer</p>
-                </div>
-                <div>
-                  <img src={ava_4} alt="img" /> 
-                </div>
-              </div>
-            </article>
-          </div>
-          <div className="cell large-4 medium-6 small-12">
-            <article className="testimonials rounded-2xl">
-              <div className="p-10">
-                <p className='text-lg codepro-reg text-purpleColor2'>"Brainwave has been a game-changer for my business. The AI chatbot helps me stay on top of industry trends and provides me with customized recommendations that help me make informed decisions. It's like having a personal assistant at my fingertips. I would highly recommend this app to any entrepreneur looking to improve their productivity and efficiency."</p>
-              </div>
-              <div className="flex justify-between px-10 py-6 testimonials_bottom">
-                <div>
-                  <p className='text-lg'>David Chen</p>
-                  <p className='text-sm text-purpleColor'>Engineer</p>
-                </div>
-                <div>
-                  <img src={ava_5} alt="img" /> 
-                </div>
-              </div>
-            </article>
-          </div>
-          <div className="cell large-4 medium-6 small-12">
-            <article className="testimonials rounded-2xl">
-              <div className="p-10">
-                <p className='text-lg codepro-reg text-purpleColor2'>"Brainwave has revolutionized the way I interact with digital assistants. The AI chatbot is able to understand my preferences and provide customized suggestions that are tailored to my needs. The app is user-friendly and intuitive, making it a joy to use."</p>
-              </div>
-              <div className="flex justify-between px-10 py-6 testimonials_bottom">
-                <div>
-                  <p className='text-lg'>David Chen</p>
-                  <p className='text-sm text-purpleColor'>Engineer</p>
-                </div>
-                <div>
-                  <img src={ava_6} alt="img" /> 
-                </div>
-              </div>
-            </article>
-          </div>
-          
+              </article>
+            </div>
+          ))}
         </div>
-
-          
-      </section>
+      )}
+    </section>
   );
 }
 
